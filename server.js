83c81a44a89737bbb01ef2c3460a6874a0cc5856
Preston Powell <prestonpowell@Prestons-MacBook-Pro.local>
2018-07-13 15:35:23 -0700

    user auth

--- server.js
+++ server.js
@@ -4,12 +4,22 @@ var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy
 var bodyParser = require('body-parser')
 var session = require('express-session')
+var User = require('./models/user')
+var path = require('path')
+var mongoose = require('mongoose')
 
 app.use(express.static('public'))
 app.use(session({ secret: 'cats' }))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(passport.initialize())
 app.use(passport.session())
+
+mongoose.connect('mongodb://localhost/webservSMS')
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', function (callback) {
+  console.log('Connection succeeded.')
+})
 
 // strategy
 passport.use(new LocalStrategy(
@@ -37,8 +47,18 @@ passport.deserializeUser(function (id, done) {
   })
 })
 
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true })
-)
+
+app.post('/login', (req, res) => {
+  var user = new User({
+    username: req.body.username,
+    password: req.body.password
+  })
+  console.log(user)
+  User.create(user)
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/login.html'))
+})
+
+app.listen(3000)
